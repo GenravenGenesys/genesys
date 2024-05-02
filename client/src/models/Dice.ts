e81fdef10b93f03d7ea9641c74611fd00ae6d344
genraven1 <genraven1@outlook.com>
2024-05-02 11:36:37 -0500

    Add Dice Values

---
+++ client/src/models/Dice.ts
@@ -0,0 +1,69 @@
+export enum Ability {
+    ONE = '',
+    TWO = '[success]',
+    THREE = '[success]',
+    FOUR = '[success] [success]',
+    FIVE = '[advantage]',
+    SIX = '[advantage]',
+    SEVEN = '[success] [advantage]',
+    EIGHT = '[advantage] [advantage]'
+}
+
+export enum Boost {
+    ONE = '',
+    TWO = '',
+    THREE = '[success]',
+    FOUR = '[success] [advantage]',
+    FIVE = '[advantage] [advantage]',
+    SIX = '[advantage]'
+}
+
+export enum Proficiency {
+    ONE = '',
+    TWO = '[success]',
+    THREE = '[success]',
+    FOUR = '[success] [success]',
+    FIVE = '[success] [success]',
+    SIX = '[advantage]',
+    SEVEN = '[success] [advantage]',
+    EIGHT = '[success] [advantage]',
+    NINE = '[success] [advantage]',
+    TEN = '[advantage] [advantage]',
+    ELEVEN = '[advantage] [advantage]',
+    TWELVE = '[success] [triumph]'
+}
+
+export enum Setback {
+    ONE = '',
+    TWO = '',
+    THREE = '[failure]',
+    FOUR = '[failure]',
+    FIVE = '[threat]',
+    SIX = '[threat]'
+}
+
+export enum Difficulty {
+    ONE = '',
+    TWO = '[failure]',
+    THREE = '[failure] [failure]',
+    FOUR = '[threat]',
+    FIVE = '[threat]',
+    SIX = '[threat]',
+    SEVEN = '[threat] [threat]',
+    EIGHT = '[failure]'
+}
+
+export enum Challenge {
+    ONE = '',
+    TWO = '[failure]',
+    THREE = '[failure]',
+    FOUR = '[failure] [failure]',
+    FIVE = '[failure] [failure]',
+    SIX = '[threat]',
+    SEVEN = '[threat]',
+    EIGHT = '[failure] [threat]',
+    NINE = '[failure] [threat]',
+    TEN = '[threat] [threat]',
+    ELEVEN = '[threat] [threat]',
+    TWELVE = '[failure] [despair]'
+}
