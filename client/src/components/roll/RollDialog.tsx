dfc3fc4acfcb53998e73c85fd1e0d336be67dd4c
genraven1 <genraven1@outlook.com>
2024-05-07 13:23:37 -0500

    Encode Result Types and DieTypes

--- client/src/components/roll/RollDialog.tsx
+++ client/src/components/roll/RollDialog.tsx
@@ -1,4 +1,4 @@
-import Roll, {DefaultResults, Results, ResultType} from "../../models/Roll";
+import Roll, {DefaultResults, DefaultRoll, DieType, Results, ResultType} from "../../models/Roll";
 import {Fragment, useEffect, useState} from "react";
 import {
     Button,
@@ -18,12 +18,12 @@ import ViewRollTable from "./ViewRollTable";
 interface Props {
     open: boolean
     onClose: () => void
-    diceRoll: Roll
+    diceRoll?: Roll
 }
 
 export default function RollDialog(props: Props) {
     const {open, onClose, diceRoll} = props
-    const [roll, setRoll] = useState<Roll>(diceRoll)
+    const [roll, setRoll] = useState<Roll>(diceRoll || DefaultRoll.create)
     const [results, setResults] = useState<Results>(DefaultResults.create())
 
     const onClick = () => {
@@ -33,29 +33,53 @@ export default function RollDialog(props: Props) {
 
     const renderRoll = () => {
         let text = ''
-        while (results.success > 0) {
+        while (roll.proficiency > 0) {
+            text = text.concat(DieType.Proficiency + ' ')
+            roll.proficiency--
+        }
+        while (roll.ability > 0) {
+            text = text.concat(DieType.Ability + ' ')
+            roll.ability--
+        }
+        while (roll.boost > 0) {
+            text = text.concat(DieType.Boost + ' ')
+            roll.boost--
+        }
+        while (roll.challenge > 0) {
+            text = text.concat(DieType.Challenge + ' ')
+            roll.challenge--
+        }
+        while (roll.difficulty > 0) {
+            text = text.concat(DieType.Difficulty + ' ')
+            roll.difficulty--
+        }
+        while (roll.setback > 0) {
+            text = text.concat(DieType.Setback + ' ')
+            roll.setback--
+        }
+        while (roll.success > 0) {
             text = text.concat(ResultType.Success + ' ')
-            results.success--
+            roll.success--
         }
-        while (results.failure > 0) {
+        while (roll.failure > 0) {
             text = text.concat(ResultType.Failure + ' ')
-            results.failure--
+            roll.failure--
         }
-        while (results.advantage > 0) {
+        while (roll.advantage > 0) {
             text = text.concat(ResultType.Advantage + ' ')
-            results.advantage--
+            roll.advantage--
         }
-        while (results.threat > 0) {
+        while (roll.threat > 0) {
             text = text.concat(ResultType.Threat + ' ')
-            results.threat--
+            roll.threat--
         }
-        while (results.triumph > 0) {
+        while (roll.triumph > 0) {
             text = text.concat(ResultType.Triumph + ' ')
-            results.triumph--
+            roll.triumph--
         }
-        while (results.despair > 0) {
+        while (roll.despair > 0) {
             text = text.concat(ResultType.Despair + ' ')
-            results.despair--
+            roll.despair--
         }
         if (text === null || text === undefined) {
             return '';
@@ -64,17 +88,17 @@ export default function RollDialog(props: Props) {
         const array = string.map((word: string) => {
             const target = word.toLowerCase();
             switch (true) {
-                case target.includes('[boost]'):
+                case target.includes(DieType.Boost):
                     return '<i class="symbol d6 symbol-border boost-color"></i>';
-                case target.includes('[ability]'):
+                case target.includes(DieType.Ability):
                     return '<i class="symbol  d8 symbol-border ability-color"></i>';
-                case target.includes('[proficiency]'):
+                case target.includes(DieType.Proficiency):
                     return '<i class="symbol d12 symbol-border proficiency-color"></i>';
-                case target.includes('[setback]'):
+                case target.includes(DieType.Setback):
                     return '<i class="symbol d6 symbol-border setback-color"></i>';
-                case target.includes('[difficulty]'):
+                case target.includes(DieType.Difficulty):
                     return '<i class="symbol d8 symbol-border difficulty-color"></i>';
-                case target.includes('[challenge]'):
+                case target.includes(DieType.Challenge):
                     return '<i class="symbol d12 symbol-border challenge-color"></i>';
                 case target.includes(ResultType.Advantage):
                     return '<i class="symbol advantage"></i>';
