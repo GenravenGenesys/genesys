94841d8cb5b2fee038685148840cceb618fd4e3d
genraven1 <genraven1@outlook.com>
2024-09-13 10:54:55 -0500

    Add logging

--- functions/campaigns/talents/[campaign_id].ts
+++ functions/campaigns/talents/[campaign_id].ts
@@ -22,9 +22,9 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
 
 export const onRequestPost: PagesFunction<Env> = async (context) => {
     const talent = await context.request.json() as Talent;
-    console.log(talent)
+    console.log(talent.talent_id);
     console.log(context.params.campaign_id);
-    const result = await context.env.GENESYS.prepare('INSERT INTO CampaignTalent (campaign_id, talent_id) VALUES (?1, ?2);')
+    const result = await context.env.GENESYS.prepare(`INSERT INTO CampaignTalent (campaign_id, talent_id) VALUES (?1, ?2)`)
         .bind(context.params.campaign_id, talent.talent_id);
     return Response.json(result);
 }
