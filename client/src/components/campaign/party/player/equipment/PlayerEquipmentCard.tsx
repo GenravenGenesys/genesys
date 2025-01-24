13b4b0d12ee2efc92104bed045006ec699423339
genraven1 <genraven1@outlook.com>
2025-01-24 13:21:13 -0600

    Update PlayerEquipmentCard FC

--- client/src/components/campaign/party/player/equipment/PlayerEquipmentCard.tsx
+++ client/src/components/campaign/party/player/equipment/PlayerEquipmentCard.tsx
@@ -1,140 +1,101 @@
-import {Button, Card, CardContent, Grid} from "@mui/material";
-import * as React from "react";
-import Typography from "@mui/material/Typography";
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import {Fragment, useState} from "react";
 import Player from "../../../../../models/actor/player/Player";
 import PlayerArmorTable from "./armor/PlayerArmorTable";
 import PlayerWeaponTable from "./weapon/PlayerWeaponTable";
 import CenteredCardHeader from "../../../../common/card/header/CenteredCardHeader";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import PlayerWeaponSelectionDialog from "./weapon/PlayerWeaponSelectionDialog";
 import PlayerArmorSelectionDialog from "./armor/PlayerArmorSelectionDialog";
 import PlayerArmorEquipDialog from "./armor/PlayerArmorEquipDialog";
 import PlayerWeaponEquipDialog from "./weapon/PlayerWeaponEquipDialog";
+import React, {Fragment, useState} from "react";
 
 interface Props {
-    player: Player
+    player: Player;
 }
 
-export default function PlayerEquipmentCard(props: Props) {
-    const {player} = props
-    const [value, setValue] = useState('1')
-    const [openSelectWeaponDialog, setOpenSelectWeaponDialog] = useState(false)
-    const [openSelectArmorDialog, setOpenSelectArmorDialog] = useState(false)
-    const [openEquipArmorDialog, setOpenEquipArmorDialog] = useState(false)
-    const [openEquipWeaponDialog, setOpenEquipWeaponDialog] = useState(false)
-    const pathname = useLocation().pathname
+const PlayerEquipmentCard: React.FC<Props> = ({ player }) => {
+    const [value, setValue] = useState('1');
+    const [openSelectWeaponDialog, setOpenSelectWeaponDialog] = useState(false);
+    const [openSelectArmorDialog, setOpenSelectArmorDialog] = useState(false);
+    const [openEquipArmorDialog, setOpenEquipArmorDialog] = useState(false);
+    const [openEquipWeaponDialog, setOpenEquipWeaponDialog] = useState(false);
+    const pathname = useLocation().pathname;
 
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-        setValue(newValue)
-    }
+        setValue(newValue);
+    };
 
     const renderWeaponsTab = () => {
         return (
             <Fragment>
-                {renderWeaponsTable()}
-                {renderWeaponTableButtons()}
-            </Fragment>
-        )
-    }
-
-    const renderWeaponsTable = () => {
-        if (player.weapons === undefined || player.weapons.length === 0) {
-            return <Typography style={{textAlign: 'center'}}>None</Typography>
-        }
-        return <PlayerWeaponTable weapons={player.weapons} player={player}/>
-    }
-
-    const renderWeaponTableButtons = () => {
-        if (pathname.endsWith('/edit')) {
-            return (
-                <Fragment>
+                {player.weapons === undefined || player.weapons.length === 0 ?
+                    <Typography style={{ textAlign: 'center' }}>None</Typography> :
+                    <PlayerWeaponTable weapons={player.weapons} player={player} />}
+                {pathname.endsWith('/edit') && <Fragment>
                     <Button color='primary' variant='contained' onClick={(): void => setOpenSelectWeaponDialog(true)}>Add
                         Weapon</Button>
                     {openSelectWeaponDialog &&
                         <PlayerWeaponSelectionDialog player={player} open={openSelectWeaponDialog}
-                                                     onClose={(): void => setOpenSelectWeaponDialog(false)}/>}
+                                                     onClose={(): void => setOpenSelectWeaponDialog(false)} />}
                     <Button color='primary' variant='contained' onClick={(): void => setOpenEquipWeaponDialog(true)}>Equip
                         Weapon</Button>
                     {openEquipWeaponDialog && <PlayerWeaponEquipDialog player={player} open={openEquipWeaponDialog}
-                                                                     onClose={(): void => setOpenEquipWeaponDialog(false)}/>}
-                </Fragment>
-            )
-        } else {
-            return <Fragment/>
-        }
-    }
+                                                                       onClose={(): void => setOpenEquipWeaponDialog(false)} />}
+                </Fragment>}
+            </Fragment>
+        )
+    };
 
     const renderArmorTab = () => {
         return (
             <Fragment>
-                {renderArmorTable()}
-                {renderArmorTableButtons()}
-            </Fragment>
-        )
-    }
-
-    const renderArmorTable = () => {
-        if (player.armors === undefined || player.armors.length === 0) {
-            return <Typography style={{textAlign: 'center'}}>None</Typography>
-        }
-        return <PlayerArmorTable armor={player.armors}/>
-    }
-
-    const renderArmorTableButtons = () => {
-        if (pathname.endsWith('/edit')) {
-            return (
-                <Fragment>
+                {player.armors === undefined || player.armors.length === 0 ?
+                    <Typography style={{ textAlign: 'center' }}>None</Typography> :
+                    <PlayerArmorTable armor={player.armors} />}
+                {pathname.endsWith('/edit') && <Fragment>
                     <Button color='primary' variant='contained' onClick={(): void => setOpenSelectArmorDialog(true)}>Add
                         Armor</Button>
                     {openSelectArmorDialog && <PlayerArmorSelectionDialog player={player} open={openSelectArmorDialog}
-                                                                          onClose={(): void => setOpenSelectArmorDialog(false)}/>}
+                                                                          onClose={(): void => setOpenSelectArmorDialog(false)} />}
                     <Button color='primary' variant='contained' onClick={(): void => setOpenEquipArmorDialog(true)}>Equip
                         Armor</Button>
                     {openEquipArmorDialog && <PlayerArmorEquipDialog player={player} open={openEquipArmorDialog}
-                                                                     onClose={(): void => setOpenEquipArmorDialog(false)}/>}
-                </Fragment>
-            )
-        } else {
-            return <Fragment/>
-        }
-    }
+                                                                     onClose={(): void => setOpenEquipArmorDialog(false)} />}
+                </Fragment>}
+            </Fragment>
+        )
+    };
 
     const addGear = () => {
-
-    }
+        // Your add gear logic here
+    };
 
     const renderGearTab = () => {
         return (
             <Fragment>
-                {renderGearTable()}
+                {player.gear === undefined || player.gear.length === 0 ?
+                    <Typography style={{ textAlign: 'center' }}>None</Typography> : <Fragment />}
                 <Button color='primary' variant='contained' onClick={addGear}>Add Gear</Button>
             </Fragment>
         )
-    }
-
-    const renderGearTable = () => {
-        if (player.gear === undefined || player.gear.length === 0) {
-            return <Typography style={{textAlign: 'center'}}>None</Typography>
-        }
-        return <Fragment/>
-    }
+    };
 
     return (
-        <Card sx={{"width": 1}}>
-            <CenteredCardHeader title={'Equipment'}/>
+        <Card sx={{ "width": 1 }}>
+            <CenteredCardHeader title={'Equipment'} />
             <CardContent>
-                <Grid sx={{width: 1}}>
+                <Grid sx={{ width: 1 }}>
                     <TabContext value={value}>
-                        <Grid sx={{borderBottom: 1, borderColor: 'divider'}}>
-                            <TabList onChange={handleChange} aria-label="lab API tabs example">
-                                <Tab label="Weapons" value="1"/>
-                                <Tab label="Armor" value="2"/>
-                                <Tab label="Gear" value="3"/>
+                        <Grid sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleChange}>
+                                <Tab label="Weapons" value="1" />
+                                <Tab label="Armor" value="2" />
+                                <Tab label="Gear" value="3" />
                             </TabList>
                         </Grid>
                         <TabPanel value="1">{renderWeaponsTab()}</TabPanel>
@@ -145,4 +106,6 @@ export default function PlayerEquipmentCard(props: Props) {
             </CardContent>
         </Card>
     )
-}
+};
+
+export default PlayerEquipmentCard;
