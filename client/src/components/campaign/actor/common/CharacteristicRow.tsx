2e6f335b96d39c0065263fe793f589cdab90f786
genraven1 <genraven1@outlook.com>
2025-04-23 16:04:24 -0500

    Update Gird Characteristic Row

--- client/src/components/campaign/actor/common/CharacteristicRow.tsx
+++ client/src/components/campaign/actor/common/CharacteristicRow.tsx
@@ -1,10 +1,10 @@
 import Actor from "../../../../models/actor/Actor";
-import {Grid} from "@mui/material";
 import {CharacteristicType} from "../../../../models/actor/Characteristic";
 import * as React from "react";
 import {useLocation} from "react-router-dom";
 import CharacteristicCard from "../../../common/card/CharacteristicCard";
 import ViewFieldCard from "../../../common/ViewFieldCard";
+import GridContainer from "../../../common/grid/GridContainer";
 
 interface Props {
     actor: Actor;
@@ -21,14 +21,14 @@ const CharacteristicRow: React.FC<Props> = ({actor, handleCharacteristicChange})
     };
 
     return (
-        <Grid container spacing={2}>
+        <GridContainer spacing={2}>
             {renderField(CharacteristicType.Brawn, actor.brawn.current)}
             {renderField(CharacteristicType.Agility, actor.agility.current)}
             {renderField(CharacteristicType.Intellect, actor.intellect.current)}
             {renderField(CharacteristicType.Cunning, actor.cunning.current)}
             {renderField(CharacteristicType.Willpower, actor.willpower.current)}
             {renderField(CharacteristicType.Presence, actor.presence.current)}
-        </Grid>
+        </GridContainer>
     );
 };
 
