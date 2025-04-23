02c482b42a31d790c227938073d980ced3704286
genraven1 <genraven1@outlook.com>
2025-04-23 13:11:55 -0500

    Update CharacteristicCard to use common components

--- client/src/components/common/card/CharacteristicCard.tsx
+++ client/src/components/common/card/CharacteristicCard.tsx
@@ -15,7 +15,8 @@ const CharacteristicCard: React.FC<Props> = ({type, value, handleCharacteristicC
         handleCharacteristicChange(type, value);
     };
 
-    return <NumberTextFieldCard title={type} value={value} onChange={updateCharacteristic} min={1} max={5} disabled={disabled}/>;
+    return <NumberTextFieldCard title={type} value={value} onChange={updateCharacteristic} min={1} max={5}
+                                disabled={disabled}/>;
 };
 
 export default CharacteristicCard;
