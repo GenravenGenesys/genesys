5be4af10a1923735796e667ae3a7e3537f9e60cc
genraven1 <genraven1@outlook.com>
2025-05-07 09:41:25 -0500

    Update API Routing for Injuries, Qualities, and Modifiers

--- client/src/services/QualityService.ts
+++ client/src/services/QualityService.ts
@@ -1,55 +1,22 @@
 import {RootPath} from "./RootPath";
 import Quality from "../models/Quality";
+import {apiRequest, apiRequestList} from "./ApiRequest";
 
 export default class QualityService {
 
     static async getQualities(): Promise<Quality[]> {
-        return await fetch(RootPath.Qualities)
-            .then((res) => {
-                switch (res.status) {
-                    case 204:
-                        return []
-                    case 200:
-                        return res.json()
-                    default:
-                        throw new Error(res.statusText)
-                }
-            })
+        return apiRequestList(RootPath.Qualities);
     }
 
     static async getQuality(id: string): Promise<Quality> {
-        return await fetch(RootPath.Qualities + `${id}`)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(res.statusText)
-                }
-                return res.json()
-            })
+        return apiRequest(RootPath.Qualities + `${id}`);
     }
 
     static async createQuality(name: string): Promise<Quality> {
-        return await fetch(RootPath.Qualities + `${name}`, {method: "POST"})
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(res.statusText)
-                }
-                return res.json()
-            })
+        return apiRequest(RootPath.Qualities + `${name}`, "POST");
     }
 
     static async updateQuality(quality: Quality): Promise<Quality> {
-        return await fetch(RootPath.Qualities + `${quality.id}`, {
-            method: "PUT",
-            body: JSON.stringify(quality),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(res.statusText)
-                }
-                return res.json()
-            })
+        return apiRequest(RootPath.Qualities + `${quality.id}`, "PUT", quality);
     }
 }
