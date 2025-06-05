170afe9ab09724057aadaeda8d790b5f9cb1457f
genraven1 <genraven1@outlook.com>
2025-06-05 17:42:02 -0500

    Add Auth

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,25 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from '../src/components/home/App';
 import { version } from 'react';
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { AuthProvider } from 'react-oidc-context';
+
+const oidcConfig = {
+    authority: "http://localhost:8081/realms/genesys",
+    client_id: "genesys-react-app",
+    redirect_uri: "http://localhost:8080/",
+    // ...
+};
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
     const root = createRoot(rootElement!);
-    root.render(<App />);
+    root.render(
+        <AuthProvider {...oidcConfig}>
+            <App />
+        </AuthProvider>
+    );
 } else {
     console.error("Error: Root element not found!");
 }
