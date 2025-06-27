03f28c53a20d6eec89ca61647c25cb58242a2a35
genraven1 <genraven1@gmail.com>
2025-06-27 11:26:16 -0500

    Update NPC Endpoints (#300)
    * Update Endpoints to NPC
    
    * Adding Mapstruct
    
    * Add React Query
    
    * Add Query Provider
    
    * Add Rival Logging
    
    * Update Rival Page
    
    * Update Number Field Card
    
    * Update formatting
    
    * Update Rating Card
    
    * Update Rival Java Object

--- client/src/components/campaign/npc/rival/RivalCharacteristicTab.tsx
+++ client/src/components/campaign/npc/rival/RivalCharacteristicTab.tsx
@@ -1,124 +1,118 @@
-import {Divider} from "@mui/material";
+import { Divider } from "@mui/material";
 import ViewFieldCard from "../../../common/ViewFieldCard";
-import {DefenseType} from "../../../../models/actor/Defense";
+import { DefenseType } from "../../../../models/actor/Defense";
 import * as React from "react";
 import Rival from "../../../../models/actor/npc/Rival";
 import RatingCard from "../RatingCard";
-import {RatingType} from "../../../../models/actor/npc/NonPlayerActor";
-import {Fragment} from "react";
+import { RatingType } from "../../../../models/actor/npc/NonPlayerActor";
+import { Fragment, useEffect } from "react";
 import CharacteristicRow from "../../actor/common/CharacteristicRow";
 import NumberTextFieldCard from "../../../common/card/NumberTextFieldCard";
-import {StatsType} from "../../../../models/actor/Stats";
-import {CharacteristicType} from "../../../../models/actor/Characteristic";
+import { StatsType } from "../../../../models/actor/Stats";
+import { CharacteristicType } from "../../../../models/actor/Characteristic";
 import RivalService from "../../../../services/actor/RivalService";
-import {useLocation} from "react-router";
+import { useLocation } from "react-router";
 import GridContainer from "../../../common/grid/GridContainer";
 
 interface Props {
     rival: Rival;
-    updateRival: (rival: Rival) => void;
+    refetch: () => void;
 }
 
-const RivalCharacteristicTab: React.FC<Props> = ({rival, updateRival})=> {
+const RivalCharacteristicTab: React.FC<Props> = ({ rival, refetch }) => {
     let pathname = useLocation().pathname;
+    const isEdit = pathname.endsWith(`${rival.id}/edit`);
+
+    useEffect(() => {
+        console.log("RivalCharacteristicTab mounted with rival:", rival);
+    }, [rival]);
 
     const handleCharacteristicChange = async (characteristic: CharacteristicType, value: number) => {
-        if (rival) {
-            switch (characteristic) {
-                case CharacteristicType.Brawn:
-                    updateRival(await RivalService.updateRival({...rival, brawn: {...rival.brawn, current: value}}));
-                    break;
-                case CharacteristicType.Agility:
-                    updateRival(await RivalService.updateRival({
-                        ...rival,
-                        agility: {...rival.agility, current: value}
-                    }));
-                    break;
-                case CharacteristicType.Intellect:
-                    updateRival(await RivalService.updateRival({
-                        ...rival,
-                        intellect: {...rival.intellect, current: value}
-                    }));
-                    break;
-                case CharacteristicType.Cunning:
-                    updateRival(await RivalService.updateRival({
-                        ...rival,
-                        cunning: {...rival.cunning, current: value}
-                    }));
-                    break;
-                case CharacteristicType.Willpower:
-                    updateRival(await RivalService.updateRival({
-                        ...rival,
-                        willpower: {...rival.willpower, current: value}
-                    }));
-                    break;
-                case CharacteristicType.Presence:
-                    updateRival(await RivalService.updateRival({
-                        ...rival,
-                        presence: {...rival.presence, current: value}
-                    }));
-                    break;
-            }
+        if (!rival) return;
+
+        const updated = { ...rival };
+
+        switch (characteristic) {
+            case CharacteristicType.Brawn:
+                updated.brawn.current = value;
+                break;
+            case CharacteristicType.Agility:
+                updated.agility.current = value;
+                break;
+            case CharacteristicType.Intellect:
+                updated.intellect.current = value;
+                break;
+            case CharacteristicType.Cunning:
+                updated.cunning.current = value;
+                break;
+            case CharacteristicType.Willpower:
+                updated.willpower.current = value;
+                break;
+            case CharacteristicType.Presence:
+                updated.presence.current = value;
+                break;
         }
+
+        await RivalService.updateRival(updated);
+        refetch();
     };
 
     const handleWoundsChange = async (value: number) => {
-        if (rival) {
-            updateRival(await RivalService.updateRival({
-                ...rival,
-                wounds: {current: rival.wounds.current, threshold: value, type: rival.wounds.type}
-            }));
-        }
+        if (!rival) return;
+
+        const updated = {
+            ...rival,
+            wounds: { ...rival.wounds, threshold: value }
+        };
+
+        await RivalService.updateRival(updated);
+        refetch();
     };
 
     const handleRatingsChange = async (value: number, type: RatingType) => {
-        if (rival) {
-            switch (type) {
-                case RatingType.Combat:
-                    updateRival(await RivalService.updateRival({...rival, combat: value}));
-                    break;
-                case RatingType.Social:
-                    updateRival(await RivalService.updateRival({...rival, social: value}));
-                    break;
-                case RatingType.General:
-                    updateRival(await RivalService.updateRival({...rival, general: value}));
-                    break;
-            }
-        }
-    };
+        if (!rival) return;
 
-    const renderRatingRow = () => {
-        if (pathname.endsWith(rival.id + '/edit')) {
-            return (
-                <GridContainer spacing={2}>
-                    <RatingCard type={RatingType.Combat} value={rival.combat}
-                                onChange={handleRatingsChange}
-                                disabled={!pathname.endsWith(rival.id + '/edit')}/>
-                    <RatingCard type={RatingType.Social} value={rival.social}
-                                onChange={handleRatingsChange}
-                                disabled={!pathname.endsWith(rival.id + '/edit')}/>
-                    <RatingCard type={RatingType.General} value={rival.general}
-                                onChange={handleRatingsChange}
-                                disabled={!pathname.endsWith(rival.id + '/edit')}/>
-                </GridContainer>
-            )
+        const updated = { ...rival };
+
+        switch (type) {
+            case RatingType.Combat:
+                updated.combat = value;
+                break;
+            case RatingType.Social:
+                updated.social = value;
+                break;
+            case RatingType.General:
+                updated.general = value;
+                break;
         }
-        return <Fragment/>
+
+        await RivalService.updateRival(updated);
+        refetch();
     };
 
     return (
         <GridContainer centered>
-            <CharacteristicRow actor={rival} handleCharacteristicChange={handleCharacteristicChange}/>
-            <Divider/>
+            <CharacteristicRow actor={rival} handleCharacteristicChange={handleCharacteristicChange} />
+            <Divider />
             <GridContainer spacing={2}>
-                <ViewFieldCard name={'Soak'} value={String(rival.soak)}/>
+                <ViewFieldCard name={'Soak'} value={String(rival.soak)} />
                 <NumberTextFieldCard title={StatsType.Wounds + ' Threshold'} value={rival.wounds.threshold}
-                                     onChange={handleWoundsChange} min={1} max={20}
-                                     disabled={pathname.endsWith(rival.id + '/edit')}/>
-                <ViewFieldCard name={DefenseType.Melee} value={String(rival.melee)}/>
-                <ViewFieldCard name={DefenseType.Ranged} value={String(rival.ranged)}/>
+                    onChange={handleWoundsChange} min={1} max={20}
+                    disabled={!isEdit} />
+                <ViewFieldCard name={DefenseType.Melee} value={String(rival.melee)} />
+                <ViewFieldCard name={DefenseType.Ranged} value={String(rival.ranged)} />
             </GridContainer>
-            {renderRatingRow()}
+            {isEdit && <GridContainer spacing={2}>
+                <RatingCard type={RatingType.Combat} value={rival.combat}
+                    onChange={handleRatingsChange}
+                    disabled={!isEdit} />
+                <RatingCard type={RatingType.Social} value={rival.social}
+                    onChange={handleRatingsChange}
+                    disabled={!isEdit} />
+                <RatingCard type={RatingType.General} value={rival.general}
+                    onChange={handleRatingsChange}
+                    disabled={!isEdit} />
+            </GridContainer>}
         </GridContainer>
     );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,21 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from '../src/components/home/App';
-import React from 'react';
-import { version } from 'react';
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const rootElement = document.getElementById("root");
+const queryClient = new QueryClient();
 
 if (rootElement) {
-    const root = createRoot(rootElement!);
-    root.render(<App />);
+    createRoot(rootElement!).render(
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    );
 } else {
     console.error("Error: Root element not found!");
 }
-
-
-console.log(version);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
