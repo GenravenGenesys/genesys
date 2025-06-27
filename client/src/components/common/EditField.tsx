2a65579078bbf3903da9be6c324295d49daeee21
genraven1 <genraven1@outlook.com>
2025-06-27 11:13:49 -0500

    Update Rating Card

--- client/src/components/common/EditField.tsx
+++ client/src/components/common/EditField.tsx
@@ -1,58 +1,58 @@
 import { Box, IconButton } from "@mui/material";
-import EditIcon from '@mui/icons-material/Edit';
-import CancelIcon from '@mui/icons-material/Cancel';
-import CheckIcon from '@mui/icons-material/Check';
+import EditIcon from "@mui/icons-material/Edit";
+import CancelIcon from "@mui/icons-material/Cancel";
+import CheckIcon from "@mui/icons-material/Check";
 import { ReactNode } from "react";
-import GridContainer from "./grid/GridContainer";
 import GridItem from "./grid/GridItem";
 
 interface Props {
-    edit: boolean
-    editable?: boolean
-    viewElement: ReactNode | null
-    editElement: ReactNode | null
-    onEdit: () => void
-    onCancel: () => void
-    onCommit: () => void
+    edit: boolean;
+    editable?: boolean;
+    viewElement: ReactNode | null;
+    editElement: ReactNode | null;
+    onEdit: () => void;
+    onCancel: () => void;
+    onCommit: () => void;
 }
 
-export default function EditField(props: Props): JSX.Element {
-    const { edit, editable, viewElement, editElement, onEdit, onCancel, onCommit } = props
-    let isEditable = editable ?? true
-
+const EditField = ({
+    edit,
+    editable = true,
+    viewElement,
+    editElement,
+    onEdit,
+    onCancel,
+    onCommit
+}: Props) => {
     const editButton = (
-        <IconButton title='Edit' size='small' onClick={(): void => onEdit()}>
-            <EditIcon color='primary' fontSize='small' />
+        <IconButton title="Edit" size="small" onClick={onEdit}>
+            <EditIcon color="primary" fontSize="small" />
         </IconButton>
-    )
+    );
 
     const commitButton = (
-        <Box component='span'>
-            <IconButton title='Commit' size='small' onClick={(): void => onCommit()}>
-                <CheckIcon color='primary' fontSize='small' />
+        <Box display="flex" gap={0.5}>
+            <IconButton title="Commit" size="small" onClick={onCommit}>
+                <CheckIcon color="primary" fontSize="small" />
             </IconButton>
-            <IconButton title='Cancel' size='small' onClick={(): void => onCancel()}>
-                <CancelIcon color='primary' fontSize='small' />
+            <IconButton title="Cancel" size="small" onClick={onCancel}>
+                <CancelIcon color="primary" fontSize="small" />
             </IconButton>
         </Box>
-    )
+    );
 
-    if (!isEditable) {
-        return (
-            <GridItem>
-                {viewElement}
-            </GridItem>
-        )
-    }
+    if (!editable) {
+        return <GridItem>{viewElement}</GridItem>;
+    };
 
     return (
-        <GridContainer centered>
-            <GridItem>
-                {edit ? editElement : viewElement}
-            </GridItem>
-            <GridItem>
+        <GridItem>
+            <Box display="flex" alignItems="center" width="100%" gap={1}>
+                <Box flexGrow={1}>{edit ? editElement : viewElement}</Box>
                 {edit ? commitButton : editButton}
-            </GridItem>
-        </GridContainer>
-    )
-}
+            </Box>
+        </GridItem>
+    );
+};
+
+export default EditField;
