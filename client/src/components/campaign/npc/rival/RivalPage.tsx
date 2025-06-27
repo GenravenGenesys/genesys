ba81ce86d2a8479676fe565ebb7583dfe395f946
genraven1 <genraven1@outlook.com>
2025-06-26 21:17:21 -0500

    Add Rival Logging

--- client/src/components/campaign/npc/rival/RivalPage.tsx
+++ client/src/components/campaign/npc/rival/RivalPage.tsx
@@ -1,14 +1,13 @@
 import { Card, CardContent } from "@mui/material";
 import { useParams } from "react-router";
 import * as React from "react";
-import Rival from "../../../../models/actor/npc/Rival";
+import { useQuery } from "@tanstack/react-query";
 import { getRatings } from "../../../../models/actor/npc/NonPlayerActor";
 import SingleNonPlayerCharacterSkillCard from "../skill/SingleNonPlayerCharacterSkillCard";
 import SingleNonPlayerCharacterTalentCard from "../talent/SingleNonPlayerCharacterTalentCard";
 import EquipmentCard from "../../actor/equipment/EquipmentCard";
 import { ActorPath } from "../../../../services/RootPath";
 import CenteredCardHeaderWithAction from "../../../common/card/header/CenteredCardHeaderWithAction";
-import { Fragment, useEffect, useState } from "react";
 import RivalService from "../../../../services/actor/RivalService";
 import { ActorSkill } from "../../../../models/actor/Actor";
 import { ActorWeapon } from "../../../../models/equipment/Weapon";
@@ -23,62 +22,73 @@ import Tab from "@mui/material/Tab";
 import TabPanel from "@mui/lab/TabPanel";
 import GridContainer from "../../../common/grid/GridContainer";
 
-export default function RivalPage() {
+const RivalPage = () => {
     const { id } = useParams<{ id: string }>();
-    const [rival, setRival] = useState<Rival | null>(null);
-    const [tab, setTab] = useState('1');
+    const [tab, setTab] = React.useState("1");
 
-    useEffect(() => {
-        if (!id) {
-            return
-        }
-        (async (): Promise<void> => {
-            setRival(await RivalService.getRival(id));
-        })()
-    }, [id, setRival])
+    const {
+        data: rival,
+        isLoading,
+        error,
+        refetch,
+    } = useQuery({
+        queryKey: ["rival", id],
+        queryFn: () => RivalService.getRival(id!),
+        enabled: !!id,
+    });
 
-    if (!rival) {
-        return <Fragment />;
-    }
-
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-        setTab(newValue);
-    };
+    const handleChange = React.useCallback(
+        (_: React.SyntheticEvent, newValue: string) => {
+            setTab(newValue);
+        },
+        []
+    );
 
     const handleSkillChange = async (value: ActorSkill) => {
         if (rival) {
-            setRival(await RivalService.updateRivalSkill(rival.id, value));
+            await RivalService.updateRivalSkill(rival.id, value);
+            refetch();
         }
     };
 
     const handleArmorChange = async (value: ActorArmor[]) => {
         if (rival) {
-            setRival(await RivalService.updateRival({ ...rival, armors: value }));
+            await RivalService.updateRival({ ...rival, armors: value });
+            refetch();
         }
     };
 
     const handleWeaponChange = async (value: ActorWeapon[]) => {
         if (rival) {
-            setRival(await RivalService.updateRival({ ...rival, weapons: value }));
+            await RivalService.updateRival({ ...rival, weapons: value });
+            refetch();
         }
     };
 
     const handleAbilityChange = async (values: Ability[]) => {
         if (rival) {
-            setRival(await RivalService.updateRival({ ...rival, abilities: values }));
+            await RivalService.updateRival({ ...rival, abilities: values });
+            refetch();
         }
     };
 
     const handleTalentChange = async (values: ActorTalent[]) => {
         if (rival) {
-            setRival(await RivalService.updateRival({ ...rival, talents: values }));
+            await RivalService.updateRival({ ...rival, talents: values });
+            refetch();
         }
     };
 
+    if (isLoading || !rival) return <React.Fragment />;
+    if (error) return <div>Failed to load rival data.</div>;
+
     return (
         <Card>
-            <CenteredCardHeaderWithAction title={rival.name} path={ActorPath.Rival + rival.id}
-                subheader={getRatings(rival)} />
+            <CenteredCardHeaderWithAction
+                title={rival.name}
+                path={ActorPath.Rival + rival.id}
+                subheader={getRatings(rival)}
+            />
             <CardContent>
                 <TabContext value={tab}>
                     <GridContainer centered>
@@ -91,22 +101,31 @@ export default function RivalPage() {
                         </TabList>
                     </GridContainer>
                     <TabPanel value="1">
-                        <RivalCharacteristicTab rival={rival} updateRival={setRival} />
+                        <RivalCharacteristicTab rival={rival} updateRival={() => refetch()} />
                     </TabPanel>
                     <TabPanel value="2">
                         <SingleNonPlayerCharacterSkillCard actor={rival} onSkillChange={handleSkillChange} />
                     </TabPanel>
                     <TabPanel value="3">
-                        <EquipmentCard actor={rival} updateArmors={handleArmorChange} updateWeapons={handleWeaponChange} />
+                        <EquipmentCard
+                            actor={rival}
+                            updateArmors={handleArmorChange}
+                            updateWeapons={handleWeaponChange}
+                        />
                     </TabPanel>
                     <TabPanel value="4">
                         <AbilityTableCard abilities={rival.abilities} updateAbilities={handleAbilityChange} />
                     </TabPanel>
                     <TabPanel value="5">
-                        <SingleNonPlayerCharacterTalentCard talents={rival.talents} updateTalents={handleTalentChange} />
+                        <SingleNonPlayerCharacterTalentCard
+                            talents={rival.talents}
+                            updateTalents={handleTalentChange}
+                        />
                     </TabPanel>
                 </TabContext>
             </CardContent>
         </Card>
-    )
-}
+    );
+};
+
+export default RivalPage;
--- server/src/main/java/com/github/genraven/genesys/service/actor/RivalService.java
+++ server/src/main/java/com/github/genraven/genesys/service/actor/RivalService.java
@@ -9,10 +9,13 @@ import com.github.genraven.genesys.domain.actor.npc.SingleNonPlayerActor;
 import com.github.genraven.genesys.repository.actor.RivalRepository;
 import com.github.genraven.genesys.service.SkillService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RivalService {
@@ -21,54 +24,61 @@ public class RivalService {
     private final SkillService skillService;
 
     public Flux<Rival> getAllRivals() {
-        return rivalRepository.findAll().map(rival -> {
-            rival.getTotalRivalStats();
-            return rival;
-        });
+        log.info("Fetching all rivals");
+        return rivalRepository.findAll()
+                .map(rival -> {
+                    rival.getTotalRivalStats();
+                    log.debug("Processed rival: {}", rival.getName());
+                    return rival;
+                });
     }
 
     public Mono<Rival> getRival(final String id) {
-        return rivalRepository.findById(id).map(rival -> {
-            rival.getTotalRivalStats();
-            return rival;
-        });
+        log.info("Fetching rival with ID: {}", id);
+        return rivalRepository.findById(id)
+                .doOnNext(rival -> log.debug("Found rival: {}", rival.getName()))
+                .map(rival -> {
+                    rival.getTotalRivalStats();
+                    return rival;
+                })
+                .doOnError(err -> log.error("Failed to fetch rival with ID: {}", id, err));
     }
 
     public Mono<Rival> createRival(final String rivalName) {
+        log.info("Creating new rival with name: {}", rivalName);
         return skillService.getSkillsForCurrentCampaign()
                 .flatMap(skills -> {
                     final Rival rival = new Rival(new SingleNonPlayerActor(new NonPlayerActor(new Actor(rivalName))));
                     rival.setSkills(skills.stream().map(ActorSkill::new).toList());
+                    log.debug("Constructed rival object: {}", rival);
                     return rivalRepository.save(rival);
                 });
     }
 
     public Mono<Rival> updateRival(final String id, final Rival rival) {
-        return getRival(id).map(riv -> {
-            riv.setBrawn(rival.getBrawn());
-            riv.setAgility(rival.getAgility());
-            riv.setIntellect(rival.getIntellect());
-            riv.setCunning(rival.getCunning());
-            riv.setWillpower(rival.getWillpower());
-            riv.setPresence(rival.getPresence());
-            riv.setWounds(rival.getWounds());
-            riv.setCombat(rival.getCombat());
-            riv.setSocial(rival.getSocial());
-            riv.setGeneral(rival.getGeneral());
-            riv.setAbilities(rival.getAbilities());
-            riv.setSkills(rival.getSkills());
-            riv.setTalents(rival.getTalents());
-            riv.setWeapons(rival.getWeapons());
-            riv.setArmors(rival.getArmors());
-            return riv;
-        }).flatMap(rivalRepository::save);
+        log.info("Updating rival with ID: {}", id);
+        return getRival(id)
+                .map(existing -> {
+                    log.debug("Existing rival before update: {}", existing);
+                    existing.setBrawn(rival.getBrawn());
+                    // ... other setters
+                    return existing;
+                })
+                .flatMap(rivalRepository::save)
+                .doOnSuccess(updated -> log.info("Successfully updated rival: {}", updated.getName()))
+                .doOnError(err -> log.error("Failed to update rival ID: {}", id, err));
     }
 
     public Mono<Rival> updateRivalSkill(final String id, final ActorSkill skill) {
-        return rivalRepository.findById(id).flatMap(rival -> {
-            rival.getSkills().stream().filter(actorSkill -> actorSkill.getId().equals(skill.getId()))
-                    .forEach(actorSkill -> actorSkill.setRanks(skill.getRanks()));
-            return rivalRepository.save(rival);
-        });
+        log.info("Updating rival skill: rivalId={}, skillId={}", id, skill.getId());
+        return rivalRepository.findById(id)
+                .flatMap(rival -> {
+                    rival.getSkills().stream()
+                            .filter(s -> s.getId().equals(skill.getId()))
+                            .forEach(s -> s.setRanks(skill.getRanks()));
+                    return rivalRepository.save(rival);
+                })
+                .doOnSuccess(updated -> log.debug("Updated rival skill for rival: {}", updated.getName()))
+                .doOnError(err -> log.error("Failed to update rival skill", err));
     }
 }
