0f610f797021a62ee041d9935535f0591d098d36
genraven1 <genraven1@gmail.com>
2025-07-02 14:50:56 -0500

    Migrate Player Routes to New Classes

---
+++ server/src/main/java/com/github/genraven/genesys/handler/actor/PlayerHandler.java
@@ -0,0 +1,135 @@
+package com.github.genraven.genesys.handler.actor;
+
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.github.genraven.genesys.domain.actor.Characteristic;
+import com.github.genraven.genesys.domain.actor.player.Archetype;
+import com.github.genraven.genesys.domain.actor.player.Career;
+import com.github.genraven.genesys.domain.actor.player.Player;
+import com.github.genraven.genesys.domain.actor.player.PlayerSkill;
+import com.github.genraven.genesys.domain.talent.Talent;
+import com.github.genraven.genesys.handler.BaseHandler;
+import com.github.genraven.genesys.service.actor.PlayerService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import static com.github.genraven.genesys.constants.CommonConstants.ID;
+import static com.github.genraven.genesys.constants.CommonConstants.NAME;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+
+@Component
+@RequiredArgsConstructor
+public class PlayerHandler extends BaseHandler {
+
+    private final PlayerService playerService;
+    
+    public Mono<ServerResponse> getAllPlayers(final ServerRequest serverRequest) {
+        return playerService.getAllPlayers().collectList().flatMap(players -> {
+            if (players.isEmpty()) {
+                return ServerResponse.noContent().build();
+            }
+            return ServerResponse.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(fromValue(players));
+        });
+    }
+
+    public Mono<ServerResponse> getPlayer(final ServerRequest serverRequest) {
+        final String name = serverRequest.pathVariable(NAME);
+        return playerService.getPlayer(name)
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> createPlayer(final ServerRequest serverRequest) {
+        return playerService.createPlayer(serverRequest.pathVariable("playerName"))
+                .flatMap(player -> ServerResponse.created(getURI(player.getName())).bodyValue(player));
+    }
+
+    public Mono<ServerResponse> updatePlayer(final ServerRequest serverRequest) {
+        final String name = serverRequest.pathVariable(NAME);
+        final Mono<Player> playerMono = serverRequest.bodyToMono(Player.class);
+        return playerMono
+                .flatMap(player -> playerService.updatePlayer(name, player))
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> updatePlayerCareer(final ServerRequest serverRequest) {
+        final String id = serverRequest.pathVariable(ID);
+        final Mono<Career> careerMono = serverRequest.bodyToMono(Career.class);
+        return careerMono
+                .flatMap(career -> playerService.updatePlayerCareer(id, career))
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> updatePlayerCareerSkills(final ServerRequest serverRequest) {
+        final String id = serverRequest.pathVariable(ID);
+        final Mono<List<PlayerSkill>> skillsMono = serverRequest.bodyToMono(new ParameterizedTypeReference<>() {
+        });
+        return skillsMono
+                .flatMap(skills -> playerService.updatePlayerCareerSkills(id, skills))
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> updatePlayerArchetype(final ServerRequest serverRequest) {
+        final String id = serverRequest.pathVariable(ID);
+        final Mono<Archetype> archetypeMono = serverRequest.bodyToMono(Archetype.class);
+        return archetypeMono
+                .flatMap(archetype -> playerService.updatePlayerArchetype(id, archetype))
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> updatePlayerCharacteristic(final ServerRequest serverRequest) {
+        final String id = serverRequest.pathVariable(ID);
+        final Mono<Characteristic> characteristicMono = serverRequest.bodyToMono(Characteristic.class);
+        return characteristicMono
+                .flatMap(characteristic -> playerService.updatePlayerCharacteristic(id, characteristic))
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> updatePlayerSkill(final ServerRequest serverRequest) {
+        final String id = serverRequest.pathVariable(ID);
+        final Mono<PlayerSkill> playerSkillMono = serverRequest.bodyToMono(PlayerSkill.class);
+        return playerSkillMono
+                .flatMap(playerSkill -> playerService.updatePlayerSkill(id, playerSkill))
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> updatePlayerTalent(final ServerRequest serverRequest) {
+        final String id = serverRequest.pathVariable(ID);
+        final Mono<Talent> talentMono = serverRequest.bodyToMono(Talent.class);
+        return talentMono
+                .flatMap(talent -> playerService.updatePlayerTalent(id, talent))
+                .flatMap(player -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(player)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+}
---
+++ server/src/main/java/com/github/genraven/genesys/router/actor/PlayerRouter.java
@@ -0,0 +1,32 @@
+package com.github.genraven.genesys.router.actor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.github.genraven.genesys.handler.actor.PlayerHandler;
+
+@Configuration
+public class PlayerRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> actorRouterMethod(final PlayerHandler handler) {
+        return RouterFunctions.route()
+                .nest(RequestPredicates.path("/api"), builder -> builder
+                        .path("/players", playerBuilder -> playerBuilder
+                                .GET("/{name}", handler::getPlayer)
+                                .PUT("/{name}", handler::updatePlayer))
+                        .path("players/creation", playerCreationBuilder -> playerCreationBuilder
+                                .PATCH("/{id}/careers/", handler::updatePlayerCareer)
+                                .PATCH("/{id}/careers/skills/", handler::updatePlayerCareerSkills)
+                                .PATCH("/{id}/archetypes/", handler::updatePlayerArchetype)
+                                .PATCH("/{id}/characteristics/", handler::updatePlayerCharacteristic)
+                                .PATCH("/{id}/skills/", handler::updatePlayerSkill)
+                                .PATCH("/{id}/talents/", handler::updatePlayerTalent))
+                        )
+                .build();
+    }
+}
