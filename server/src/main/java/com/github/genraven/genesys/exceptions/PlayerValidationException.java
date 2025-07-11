8a247c7a392b8134fab7672b5b60274a3072d3b5
genraven1 <genraven1@gmail.com>
2025-07-11 09:10:07 -0500

    Update Status Call

--- server/src/main/java/com/github/genraven/genesys/exceptions/PlayerValidationException.java
+++ server/src/main/java/com/github/genraven/genesys/exceptions/PlayerValidationException.java
@@ -3,13 +3,15 @@ package com.github.genraven.genesys.exceptions;
 import java.util.List;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Getter
-public class PlayerValidationException extends RuntimeException {
+public class PlayerValidationException extends ResponseStatusException {
     private final List<String> errors;
 
     public PlayerValidationException(final List<String> errors) {
-        super("Player context validation failed");
+        super(HttpStatus.UNPROCESSABLE_ENTITY, "Player context validation failed");
         this.errors = errors;
     }
 }
--- server/src/main/java/com/github/genraven/genesys/exceptions/SceneValidationException.java
+++ server/src/main/java/com/github/genraven/genesys/exceptions/SceneValidationException.java
@@ -1,15 +1,17 @@
 package com.github.genraven.genesys.exceptions;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Getter
-public class SceneValidationException extends RuntimeException {
+public class SceneValidationException extends ResponseStatusException {
     private final List<String> errors;
 
     public SceneValidationException(final List<String> errors) {
-        super("Scene start context validation failed");
+        super(HttpStatus.UNPROCESSABLE_ENTITY, "Scene start context validation failed");
         this.errors = errors;
     }
 }
