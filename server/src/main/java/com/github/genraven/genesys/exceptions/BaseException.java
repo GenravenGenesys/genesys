d39daaef28e0deb18bc10d9b51facc36856b045b
genraven1 <genraven1@outlook.com>
2025-07-12 10:00:37 -0500

    Update Error Handling for Validation

--- server/src/main/java/com/github/genraven/genesys/exceptions/BaseException.java
+++ server/src/main/java/com/github/genraven/genesys/exceptions/BaseException.java
@@ -1,10 +1,18 @@
 package com.github.genraven.genesys.exceptions;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
+import com.github.genraven.genesys.domain.error.Error;
 
-public class BaseException extends ResponseStatusException {
-    public BaseException(final HttpStatusCode status, final String reason) {
+import java.util.List;
+
+@Getter
+public abstract class BaseException extends ResponseStatusException {
+    private final List<Error> errors;
+
+    public BaseException(final HttpStatusCode status, final String reason, final List<Error> errors) {
         super(status, reason);
+        this.errors = errors;
     }
 }
