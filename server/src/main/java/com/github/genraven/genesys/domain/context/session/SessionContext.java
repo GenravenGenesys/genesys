a3d540a33a29ddd6aa193f553c04feb507218302
genraven1 <genraven1@gmail.com>
2025-08-11 09:57:17 -0500

    Session Context Update

--- server/src/main/java/com/github/genraven/genesys/domain/context/session/SessionContext.java
+++ server/src/main/java/com/github/genraven/genesys/domain/context/session/SessionContext.java
@@ -1,16 +1,7 @@
 package com.github.genraven.genesys.domain.context.session;
 
 import com.github.genraven.genesys.domain.campaign.Session;
-import com.github.genraven.genesys.validator.ValidationGroups;
-
-import java.util.List;
 
 public interface SessionContext {
     Session session();
-
-    List<Object> getValidatableParts();
-
-    default List<Class<?>> getValidationGroups() {
-        return List.of(ValidationGroups.SceneStartValidation.class);
-    }
 }
