6244bd69b06e9289841bd8a759c2f6805202811a
genraven1 <genraven1@gmail.com>
2025-08-11 09:57:41 -0500

    Merge pull request #307
    * Session Context Update

--- server/src/main/java/com/github/genraven/genesys/domain/context/session/SessionStartSceneContext.java
+++ server/src/main/java/com/github/genraven/genesys/domain/context/session/SessionStartSceneContext.java
@@ -4,16 +4,5 @@ import com.github.genraven.genesys.domain.campaign.Scene;
 import com.github.genraven.genesys.domain.campaign.Session;
 import jakarta.validation.Valid;
 
-import java.util.List;
-
 public record SessionStartSceneContext(@Valid Session session, @Valid Scene scene) implements SessionContext {
-    @Override
-    public List<Object> getValidatableParts() {
-        return List.of(session, scene);
-    }
-
-    @Override
-    public List<Class<?>> getValidationGroups() {
-        return SessionContext.super.getValidationGroups();
-    }
 }
