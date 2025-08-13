36c626f98a8fd1fc747ac61ba8cb672666d6a2dc
genraven1 <genraven1@outlook.com>
2025-08-13 16:55:33 -0500

    RBAC-Auth

--- server/src/main/java/com/github/genraven/genesys/GenesysApplication.java
+++ server/src/main/java/com/github/genraven/genesys/GenesysApplication.java
@@ -1,12 +1,18 @@
 package com.github.genraven.genesys;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import static java.util.Arrays.stream;
+
+@Log4j2
 @OpenAPIDefinition(
     info = @Info(
         title = "Genesys API Documentation",
@@ -15,10 +21,36 @@ import io.swagger.v3.oas.annotations.info.Info;
     )
 )
 @SpringBootApplication(exclude={MongoAutoConfiguration.class})
+@ConfigurationPropertiesScan
 public class GenesysApplication {
 
-	public static void main(String[] args) {
+    enum DotEnv {
+        PORT,
+        CLIENT_ORIGIN_URL,
+        OKTA_OAUTH2_ISSUER,
+        OKTA_OAUTH2_AUDIENCE
+    }
+
+	public static void main(final String[] args) {
+        dotEnvSafeCheck();
+
 		SpringApplication.run(GenesysApplication.class, args);
 	}
+
+    private static void dotEnvSafeCheck() {
+        final var dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+
+        stream(DotEnv.values())
+            .map(DotEnv::name)
+            .filter(varName -> dotenv.get(varName, "").isEmpty())
+            .findFirst()
+            .ifPresent(varName -> {
+                log.error("[Fatal] Missing or empty environment variable: {}", varName);
+
+                System.exit(1);
+            });
+    }
 
 }
---
+++ server/src/main/java/com/github/genraven/genesys/configuration/GlobalErrorHandler.java
@@ -0,0 +1,76 @@
+package com.github.genraven.genesys.configuration;
+
+import com.github.genraven.genesys.domain.common.ErrorMessage;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.joselion.maybe.Maybe;
+
+import reactor.core.publisher.Mono;
+
+@Order(-2)
+@Component
+public record GlobalErrorHandler(ObjectMapper mapper) implements ErrorWebExceptionHandler {
+
+    @Override
+    public Mono<Void> handle(final ServerWebExchange exchange, final Throwable error) {
+        final var response = exchange.getResponse();
+        final var bufferFactory = response.bufferFactory();
+
+        if (error instanceof ResponseStatusException statusError) {
+            final var status = statusError.getStatusCode();
+            response.setStatusCode(status);
+
+            if (status.equals(HttpStatus.NOT_FOUND)) {
+                final var body = this.makeBodyBytes("Not Found")
+                    .map(bufferFactory::wrap);
+
+                return response.writeWith(body);
+            }
+        }
+
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return response.writeWith(
+            this.makeBodyBytes(error.getMessage())
+                .map(bufferFactory::wrap));
+    }
+
+    public Mono<Void> handleAuthenticationError(final ServerWebExchange exchange, final AuthenticationException error) {
+        final var response = exchange.getResponse();
+        final var message = "Unauthorized. %s".formatted(error.getMessage());
+        final var body = this.makeBodyBytes(message)
+            .map(response.bufferFactory()::wrap);
+
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.writeWith(body);
+    }
+
+    public Mono<Void> handleAccessDenied(
+        final ServerWebExchange exchange,
+        final AccessDeniedException error // NOSONAR
+    ) {
+        final var response = exchange.getResponse();
+        final var body = this.makeBodyBytes("Permission denied")
+            .map(response.bufferFactory()::wrap);
+
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+        return response.writeWith(body);
+    }
+
+    private Mono<byte[]> makeBodyBytes(final String message) {
+        return Mono.create(sink -> Maybe.just(message)
+            .map(ErrorMessage::from)
+            .resolve(mapper::writeValueAsBytes)
+            .doOnSuccess(sink::success)
+            .doOnError(sink::error));
+    }
+}
+
