4afb6506b6e70ee8cc17e0c8bedcf0023eaf25cb
genraven1 <genraven1@gmail.com>
2025-11-19 10:57:06 -0600

    update Orval files

--- server/src/main/java/com/github/genraven/genesys/domain/Difficulty.java
+++ server/src/main/java/com/github/genraven/genesys/domain/Difficulty.java
@@ -1,11 +1,13 @@
 package com.github.genraven.genesys.domain;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "The task's inherent challenge to complete.")
 public enum Difficulty {
     EASY("Easy"),
     AVERAGE("Average"),
--- server/src/main/java/com/github/genraven/genesys/domain/RangeBand.java
+++ server/src/main/java/com/github/genraven/genesys/domain/RangeBand.java
@@ -1,17 +1,25 @@
 package com.github.genraven.genesys.domain;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "The distance between to points. This can apply to characters, equipment, talents, and abilities.")
 public enum RangeBand {
+    @Schema(description = "Engaged")
     ENGAGED("Engaged"),
+    @Schema(description = "Short")
     SHORT("Short"),
+    @Schema(description = "Medium")
     MEDIUM("Medium"),
+    @Schema(description = "Long")
     LONG("Long"),
+    @Schema(description = "Extreme")
     EXTREME("Extreme"),
+    @Schema(description = "Strategic")
     STRATEGIC("Strategic");
 
     @JsonValue
