eed10d22237afaa01e5e637f35249d16bca51892
genraven1 <genraven1@outlook.com>
2026-01-19 16:59:53 -0600

    Add Archetype and Career

--- server/src/main/java/com/github/genraven/genesys/service/ArchetypeService.java
+++ server/src/main/java/com/github/genraven/genesys/service/ArchetypeService.java
@@ -1,46 +1,43 @@
 package com.github.genraven.genesys.service;
 
 import com.github.genraven.genesys.domain.actor.player.Archetype;
-import com.github.genraven.genesys.repository.ArchetypeRepository;
-
+import com.github.genraven.genesys.domain.campaign.Campaign;
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class ArchetypeService {
-    private final ArchetypeRepository archetypeRepository;
+    
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
 
-    public Flux<Archetype> getAllArchetypes() {
-        return archetypeRepository.findAll();
+    public Flux<Archetype> findAllByCampaignId(final String campaignId) {
+        return reactiveMongoTemplate.findById(campaignId, Campaign.class)
+            .flatMapMany(campaign -> Flux.fromIterable(campaign.getCompendium().getArchetypes()));
     }
 
-    public Mono<Archetype> getArchetype(final String name) {
-        return archetypeRepository.findById(name);
+    public Mono<Archetype> addArchetype(final String campaignId, final Archetype archetype) {
+        archetype.setId(UUID.randomUUID().toString());
+
+        Query query = new Query(Criteria.where("id").is(campaignId));
+        Update update = new Update().push("compendium.archetypes", archetype);
+
+        return reactiveMongoTemplate.updateFirst(query, update, Campaign.class).thenReturn(archetype);
     }
 
-    public Mono<Archetype> createArchetype(final String name) {
-        return archetypeRepository.save(new Archetype(name));
-    }
+    public Mono<Archetype> updateArchetype(final String campaignId, final String archetypeId, final Archetype updatedArchetype) {
+        Query query = new Query(Criteria.where("id").is(campaignId).and("compendium.archetypes.id").is(archetypeId));
 
-    public Mono<Archetype> updateArchetype(final String name, final Archetype archetype) {
-        return getArchetype(name).map(arch -> {
-            arch.setDescription(archetype.getDescription());
-            arch.setBrawn(archetype.getBrawn());
-            arch.setAgility(archetype.getAgility());
-            arch.setIntellect(archetype.getIntellect());
-            arch.setCunning(archetype.getCunning());
-            arch.setWillpower(archetype.getWillpower());
-            arch.setPresence(archetype.getPresence());
-            arch.setWounds(archetype.getWounds());
-            arch.setStrain(archetype.getStrain());
-            arch.setExperience(archetype.getExperience());
-            arch.setSkill(archetype.getSkill());
-            arch.setAbilities(archetype.getAbilities());
-            return arch;
-        }).flatMap(archetypeRepository::save);
+        Update update = new Update().set("compendium.archetypes.$", updatedArchetype);
+
+        return reactiveMongoTemplate.updateFirst(query, update, Campaign.class).thenReturn(updatedArchetype);
     }
 }
