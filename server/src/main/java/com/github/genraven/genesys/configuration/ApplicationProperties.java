3ef610406be875462a4fe06c050c7dcd7d56544b
Garrett Crowe <genraven1@outlook.com>
2026-01-23 14:57:26 -0600

    env file

--- server/src/main/java/com/github/genraven/genesys/configuration/ApplicationProperties.java
+++
@@ -1,7 +0,0 @@
-package com.github.genraven.genesys.configuration;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "application")
-public record ApplicationProperties(String clientOriginUrl) {
-}
