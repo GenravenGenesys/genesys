4d8ff9a9622fb2242cbb3e31701b515b151c79aa
genraven1 <genraven1@gmail.com>
2026-02-10 10:35:36 -0600

    Remove old Files

--- server/src/main/java/com/github/genraven/genesys/repository/actor/RivalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.genraven.genesys.repository.actor;
-
-import com.github.genraven.genesys.domain.actor.npc.Rival;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RivalRepository extends ReactiveMongoRepository<Rival, String> {
-}
--- server/src/main/java/com/github/genraven/genesys/repository/lore/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.genraven.genesys.repository.lore;
-
-import com.github.genraven.genesys.domain.lore.Organization;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrganizationRepository extends ReactiveMongoRepository<Organization, String> {
-}
