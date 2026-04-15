ff613c611a6e6f6e0542c01a117b0f6e69d2c4ee
genraven1 <genraven1@outlook.com>
2026-04-15 16:48:50 -0500

    Update Archetype Ability to use Font

--- client/src/components/home/new/party/archetype/ArchetypeAbilities.tsx
+++ client/src/components/home/new/party/archetype/ArchetypeAbilities.tsx
@@ -1,8 +1,9 @@
 import type {Archetype} from "../../../../../api/model";
-import {Card, CardContent, Chip, Stack, Typography} from "@mui/material";
+import {Card, CardContent, Chip, Stack} from "@mui/material";
 import GridContainer from "../../../../common/grid/GridContainer.tsx";
 import GridItem from "../../../../common/grid/GridItem.tsx";
 import CenteredCardHeader from "../../../../common/card/header/CenteredCardHeader.tsx";
+import GenesysDescriptionTypography from "../../../common/typography/GenesysDescriptionTypography.tsx";
 
 interface Props {
     archetype: Archetype;
@@ -25,9 +26,7 @@ export default function ArchetypeAbilities(props: Props) {
                             <Stack spacing={1} alignItems="center">
                                 <Chip label={ability.activation} size="small" color="primary" variant="outlined"/>
                                 {ability.description && (
-                                    <Typography variant="body2" color="text.secondary" textAlign="center">
-                                        {ability.description}
-                                    </Typography>
+                                    <GenesysDescriptionTypography text={ability.description} variant="body2" color="text.secondary"/>
                                 )}
                             </Stack>
                         </CardContent>
